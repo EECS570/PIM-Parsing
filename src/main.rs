d98c6f93bbc98e6c6733f793c15b39bf0c2f3c83
Gorgeous-Patrick <33488623+Gorgeous-Patrick@users.noreply.github.com>
2025-03-10 17:29:28 -0400

    test: new test

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ fn calculator1() {
     assert!(dspim::TermParser::new().parse("(22)").is_ok());
     assert!(dspim::TermParser::new().parse("((((22))))").is_ok());
     assert!(dspim::TermParser::new().parse("((22)").is_err());
+    assert!(dspim::BaseTypeParser::new().parse("int8").is_ok())
 }
 
 fn main() {
